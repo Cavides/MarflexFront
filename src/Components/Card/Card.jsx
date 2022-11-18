c07aafa65c7fa46810fe8a0a3e2f2e7ef8d91002
Cavides <camilov370@gmail.com>
2022-11-18 02:54:15 -0500

    corrigiendo v3

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../slices/cartSlice";
@@ -14,8 +13,8 @@ function Card(props) {
   const { product } = props;
 
   const handleClick = () => {
-      navigate(`ProductDetail/${product._id}`, { replace: true });
-  }
+    navigate(`ProductDetail/${product._id}`, { replace: true });
+  };
 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
@@ -25,16 +24,24 @@ function Card(props) {
 
   return (
     <div className="card">
-    <h3 className= "card__title">{product.title}</h3>
+      <h3 className="card__title">{product.title}</h3>
       <div className="card__containerImg">
-        <img src={product.imagen} className= "card__image"/>
+        <img src={product.imagen} className="card__image" />
       </div>
-      <h3 className= "card__title">{product.desc}</h3>
-      <h3 className= "card__title">${product.price}</h3>
-      
-      <div className='card__down'>
-        <button type="button" className="card__button" onClick={handleClick}>Ver</button>
-        <button type="button" className="card__button" onClick={() => handleAddToCart(product)}>Agregar a Carrito</button>
+      <h3 className="card__title">{product.desc}</h3>
+      <h3 className="card__title">${product.price}</h3>
+
+      <div className="card__down">
+        <button type="button" className="card__button" onClick={handleClick}>
+          Ver
+        </button>
+        <button
+          type="button"
+          className="card__button"
+          onClick={() => handleAddToCart(product)}
+        >
+          Agregar a Carrito
+        </button>
       </div>
     </div>
   );
@@ -42,10 +49,9 @@ function Card(props) {
 
 Card.propTypes = {
   product: PropTypes.shape(),
-}
+};
 Card.defaultProps = {
   product: {},
-}
-
+};
 
 export default Card;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,17 +2,38 @@ import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 const NavBar = () => {
-  return(
+  return (
     <div className="nav">
       <ul className="nav__menu">
-        <li className="nav__item"> <NavLink className= 'navBar__navLink' to='/'>Home</NavLink></li>
-        <li className="nav__item"><NavLink className= 'navBar__navLink' to='/products'>Productos</NavLink></li>
-        <li className="nav__item"><NavLink className= 'navBar__navLink' to='/faq'>FAQ</NavLink></li>
-        <li className="nav__item"><NavLink className= 'navBar__navLink' to='/contacto'>Contacto</NavLink></li>
-        <li className="nav__item"><NavLink className= 'navBar__navLink' to='/about'>Nosotros</NavLink></li>
+        <li className="nav__item">
+          {" "}
+          <NavLink className="navBar__navLink" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="navBar__navLink" to="/products">
+            Productos
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="navBar__navLink" to="/faq">
+            FAQ
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="navBar__navLink" to="/contacto">
+            Contacto
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink className="navBar__navLink" to="/about">
+            Nosotros
+          </NavLink>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
